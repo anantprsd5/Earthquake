da955cf9a6c9afc50d420f56a344302fc64f7b12
anantprsd5 <anantprsd5@gmail.com>
2017-02-22 20:05:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anant/earthquake/EarthquakeActivity.java
@@ -0,0 +1,33 @@
+package com.example.anant.earthquake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EarthquakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.earthquake_activity);
+
+        ArrayList<String> earthquakes = new ArrayList<>();
+        earthquakes.add("San Francisco");
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");
+
+        ListView list =(ListView)findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, earthquakes);
+        list.setAdapter(adapter);
+
+    }
+}
